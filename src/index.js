ba71b3bdc548683816752fe2c06a41bfb8f74921
Shahruh <shahruhduschanov@gmail.com>
2026-06-30 13:59:30 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
-import './index.css';
-@tailwind base;
-@tailwind components;
-@tailwind utilities;
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
